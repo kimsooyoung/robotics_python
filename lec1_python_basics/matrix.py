b722bf9eca6da67fce6f3292e85a9abc637e5465
kimsooyoung <tge1375@naver.com>
2023-01-21 15:16:47 +0900

    first commit
    Signed-off-by: kimsooyoung <tge1375@naver.com>

---
+++ lec1_python_basics/matrix.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+A = np.array([[2,4],[5,-6]])
+print(A)
+
+# rotation matrix generation func
+def rot_mat(theta):
+    return np.array([
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta), np.cos(theta)],    
+    ]) 
+
+theta = 0.5
+print(rot_mat(theta))
+
+# (2 X 2) dot (2 X 1) => (2 X 1)
+B = np.array([[2],[2]])
+D = A.dot(B)
+D_ = A @ B
+print(D)
+print(B)
+
+# Transpose
+print(A.transpose())
+# or
+print(np.transpose(A))
+
+# inverse
+inv_a = np.linalg.inv(A)
+print(inv_a)
+
+# Element-wise mult & Matrix mult
+print(inv_a * A)
+print(np.matmul(inv_a, A))
+print(inv_a.dot(A))
+
+# identity matrix
+print(np.identity(5))
