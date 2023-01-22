64a81df87805d726044d7b474724bcd76ca759e3
kimsooyoung <tge1375@naver.com>
2023-01-22 21:51:44 +0900

    dd fk done
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- lec3_dd_mobile_robot/diff_drive_fk.py
+++ lec3_dd_mobile_robot/diff_drive_fk.py
@@ -29,8 +29,8 @@ def euler_integration(tspan,z0,u):
     h = tspan[1] - tspan[0]
     x0, y0, theta0 = z0
     
-    xdot_c = v*math.cos(theta0)
-    ydot_c = v*math.sin(theta0)
+    xdot_c = v * math.cos(theta0)
+    ydot_c = v * math.sin(theta0)
     thetadot = omega
 
     x1 = x0 + xdot_c * h
@@ -39,42 +39,47 @@ def euler_integration(tspan,z0,u):
 
     return [x1, y1, theta1]
 
-#initial condition, [x0, y0, theta0]
-z0 = [0, 0, -math.pi/2]
+def motion_simulation():
+    #initial condition, [x0, y0, theta0]
+    z0 = [0, 0, -math.pi/2]
 
-#integration time step
-h = 0.1
+    #integration time step
+    h = 0.1
 
-# %%%%% the controls are v = speed and omega = direction
-# %%%%%% v = 0.5r(phidot_r + phidot_l)
-# %%%%%% omega = 0.5 (r/b)*(phitdot_r - phidot_l)
-# %%%%%% these are set below %%%%%%
-# %%%%%%% writing letters %%%%%%%
-t1 = np.arange(0,1,0.1)
-t2 = np.arange(1,2,0.1)
-t3 = np.arange(2,3,0.1)
-t = np.append(t1,t2)
-t = np.append(t,t3)
-u = np.zeros((len(t), 2))
-for i in range(0,len(t1)):
-    u[i,0] = 1
-for i in range(len(t1),len(t1)+len(t1)):
-    u[i,1] = math.pi/2
-    u[i,0] = 1
-for i in range(len(t1)+len(t2),len(t)):
-    u[i,0] = 1
+    # %%%%% the controls are v = speed and omega = direction
+    # %%%%%% v = 0.5r(phidot_r + phidot_l)
+    # %%%%%% omega = 0.5 (r/b)*(phitdot_r - phidot_l)
+    # %%%%%% these are set below %%%%%%
+    # %%%%%%% writing letters %%%%%%%
+    t1 = np.arange(0,1,0.1)
+    t2 = np.arange(1,2,0.1)
+    t3 = np.arange(2,3,0.1)
+    
+    t = np.append(t1,t2)
+    t = np.append(t,t3)
+    
+    u = np.zeros((len(t), 2))
+    
+    for i in range(0,len(t1)):
+        u[i,0] = 1
+    for i in range(len(t1),len(t1)+len(t1)):
+        u[i,1] = math.pi/2
+        u[i,0] = 1
+    for i in range(len(t1)+len(t2),len(t)):
+        u[i,0] = 1
 
-z = np.array(z0);
-for i in range(0,len(t)-1):
-    # u = [0.5, 0] #[v, omega] #test
-    z0 = euler_integration([t[i], t[i+1]],z0,[u[i,0],u[i,1]])
-    z = np.vstack([z, z0])
+    z = np.array(z0);
+    
+    for i in range(0,len(t)-1):
+        z0 = euler_integration([t[i], t[i+1]],z0,[u[i,0],u[i,1]])
+        z = np.vstack([z, z0])
+        
+    return t, z
 
-animate(t,z)
-  
 if __name__=="__main__":
     try:
-        draw_2d_mpl(circle=False)
+        timestamps, precalculated_state = motion_simulation()
+        animate(timestamps, precalculated_state)
     except Exception as e:
         print(e)
     finally:
