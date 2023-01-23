11e6109163095b64ad8a5e68c6a757ebf01754de
kimsooyoung <tge1375@naver.com>
2023-01-23 19:11:03 +0900

    Ongoing : KR comment
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- lec3_dd_mobile_robot/diff_drive_ik.py
+++ lec3_dd_mobile_robot/diff_drive_ik.py
@@ -5,14 +5,20 @@ import numpy as np
 
 class parameters:
     def __init__(self):
-        self.px = 0.05
+        # 목적지에 도달해야 하는 offset
+        # 목적지와 딱 맞게 가고 싶다면 px=0.0이어야 하는건가?
+        self.px = 0.01
         self.py = 0
         self.Kp = 100;
+        # 로봇 반지름
         self.R = 0.1
         self.pause = 0.01
         self.fps =10
 
 def ptP_to_ptC(x_p,y_p,theta,parms):
+    # x_p, y_p : 로봇이 도달해야 할 좌표
+    # parms.px,parms.py : 로봇이 회전 후 가져야 할 offset
+    # 실제 해당 offset 만큼은 여유를 둬야 하므로 -np.matmul이 되었다.
     cos = np.cos(theta)
     sin = np.sin(theta)
     R = np.array([[cos, -sin],
@@ -107,28 +113,39 @@ y_ref = y_center+a*np.sin(2*math.pi*t/tend)**3
 # plt.gca().set_aspect('equal')
 # plt.show()
 
-theta0 = math.pi/2; #guessed to be zero. Can be made better by specifying a better value based on the specific curve
-c = ptP_to_ptC(x_ref[0],y_ref[0],theta0,parms); #given the x_ref(1) and y_ref(1) (position of E at start) this step computes the correct starting position for the car (i.e., point P)
-x0 = c[0]
-y0 = c[1]
-z0 = np.array([x0, y0, theta0]); #x, y, theta at the start for point C
-z = np.array(z0);
-p0 = [x_ref[0],y_ref[0]]
-p = np.array(p0);
-e = np.array([0,0]) ;
+# guessed to be zero. Can be made better by 
+# specifying a better value based on the specific curve
+# 초기 로봇이 바라보는 각도
+theta0 = 1.0; 
+# given the x_ref(1) and y_ref(1) (position of E at start) 
+# this step computes the correct starting position 
+# for the car (i.e., point P)
+
+# 로봇의 offset을 반영하여 가야 할 초기 좌표를 변형한다.
+c = ptP_to_ptC(x_ref[0],y_ref[0],theta0,parms); 
+# 로봇이 도달해야 하는 좌표 
+# (world frame을 갖는다. x_ref, y_ref에서 offset 뺀값)
+x0, y0 = c
+
+# x, y, theta at the start for point C
+z0 = np.array([x0, y0, theta0]); 
+z = np.array(z0)
+
+# 도달해야 할 point들의 arr 준비
+p = np.array([x_ref[0],y_ref[0]])
+# error 값들이 쌓이게 될 arr
+e = np.array([0,0])
+# control signals 
 v = [];
 omega = [];
 
 for i in range(0,len(t)-1):
-    # 1. get x_c,y_c position
-    x_c = z0[0];
-    y_c = z0[1];
-    theta = z0[2];
+    # 1. get x_c, y_c position
+    x_c, y_c, theta = z0;
 
     # 2. get x_p, y_p from x_c,y_c
     p0 = ptC_to_ptP(z0[0],z0[1],z0[2],parms);
-    x_p = p0[0]
-    y_p = p0[1]
+    x_p, y_p = p0
 
     #% 3. get error = xe-x_ref and ye-y_ref
     error = [x_ref[i+1]-x_p, y_ref[i+1]-y_p];
