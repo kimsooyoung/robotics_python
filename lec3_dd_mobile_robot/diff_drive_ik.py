19f335d3c77c5d5b8ccf498c266b611422fce8bf
kimsooyoung <tge1375@naver.com>
2023-01-24 16:18:18 +0900

    dd ik done

--- lec3_dd_mobile_robot/diff_drive_ik.py
+++ lec3_dd_mobile_robot/diff_drive_ik.py
@@ -1,11 +1,7 @@
 from matplotlib import pyplot as plt
 import math
-from scipy import interpolate
+import dd_helper
 import numpy as np
-
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
 
 class parameters:
     def __init__(self):
@@ -20,37 +16,16 @@ class parameters:
         self.fps =10
         self.t_length = 10
 
-def ptP_to_ptC(x_p,y_p,theta,params):
-    # x_p, y_p : 로봇이 도달해야 할 좌표
-    # params.px,params.py : 로봇이 회전 후 가져야 할 offset
-    # 실제 해당 offset 만큼은 여유를 둬야 하므로 -np.matmul이 되었다.
-    cos = np.cos(theta)
-    sin = np.sin(theta)
-    R = np.array([[cos, -sin],
-                  [sin, cos]])
-    r = np.array([params.px,params.py])
-    p = np.array([x_p,y_p])
-    c = -np.matmul(R,np.transpose(r)) + np.transpose(p)
-    return c
+def animate(params, t_interp, z, p, plot_items):
 
-def ptC_to_ptP(x_c,y_c,theta,params):
-    cos = np.cos(theta)
-    sin = np.sin(theta)
-    R = np.array([[cos, -sin],
-                  [sin, cos]])
-    r = np.array([params.px,params.py])
-    c = np.array([x_c,y_c])
-    p = np.matmul(R,np.transpose(r)) + np.transpose(c)
-    return p
+    fig2, _ = plt.subplots()
 
-
-def animate(t, z, p, params):
     R = params.R;
     phi = np.arange(0,2*np.pi,0.25)
     x_circle = R*np.cos(phi)
     y_circle = R*np.sin(phi)
 
-    for i in range(0,len(t)):
+    for i in range(0,len(t_interp)):
         x = z[i,0]
         y = z[i,1]
         theta = z[i,2]
@@ -75,31 +50,25 @@ def animate(t, z, p, params):
         robot.remove()
         shape.remove()
 
-plt.close()
+    plt.close()
 
+    fig3, (ax3, ax4, ax5) = plt.subplots(nrows=3, ncols=1)
+    e, v, omega = plot_items
+    t = np.arange(0, params.t_length, 0.01)
+    ax3.set_title("Error (x_ref - x_p)")
+    ax3.plot(t, e[:, 0], color="green", label='X err')
+    ax3.plot(t, e[:, 1], color="orange", label='Y err')
+    ax3.legend(loc="upper right")
 
-def euler_integration(tspan,z0,u):
-    v = u[0]
-    omega = u[1]
-    h = tspan[1]-tspan[0]
+    ax4.plot(t, v, color="blue")
+    ax4.set_title("Control Signal - V")
 
-    x0 = z0[0]
-    y0 = z0[1]
-    theta0 = z0[2]
+    ax5.plot(t, omega, color="red")
+    ax5.set_title("Control Signal - W")
 
-    xdot_c = v*math.cos(theta0)
-    ydot_c = v*math.sin(theta0)
-    thetadot = omega
+    plt.show()
 
-    x1 = x0 + xdot_c*h
-    y1 = y0 + ydot_c*h
-    theta1 = theta0 + thetadot*h
-
-    z1 = [x1, y1, theta1]
-    return z1
-
-
-def generate_path(params=None, path_type="astroid", show_path=False):
+def generate_path(params, path_type="astroid", show_path=False):
     t0 = 0;
     tend = params.t_length;
     t = np.arange(t0,tend,0.01)
@@ -118,107 +87,91 @@ def generate_path(params=None, path_type="astroid", show_path=False):
         y_ref = y_center + a * np.sin(2 * math.pi * t/tend) **3
         
     if show_path == True:
-        plt.plot(x_ref,y_ref)
+        fig1, ax1 = plt.subplots()
+        ax1.plot(x_ref,y_ref)
+        ax1.set_title("Object Path")
+
+        plt.xlim(-2,2)
+        plt.ylim(-2,2)
         plt.gca().set_aspect('equal')
-        plt.show()
-        plt.pause(1.0)
-        plt.close()
 
     return x_ref, y_ref
 
-
-def motion_simulation(params, path=None):
-    # guessed to be zero. Can be made better by 
-    # specifying a better value based on the specific curve
-    # 초기 로봇이 바라보는 각도
-    theta0 = np.pi / 2
+def motion_simulation(params, path):
 
     x_ref, y_ref = path
     t = np.arange(0, params.t_length, 0.01)
 
-    # 로봇의 offset을 반영하여 가야 할 초기 좌표를 변형한다.
-    # 로봇이 도달해야 하는 좌표 
-    # (world frame을 갖는다. x_ref, y_ref에서 offset 뺀값)
-    z0 = ptP_to_ptC(x_ref[0],y_ref[0], theta0, params)
+    # initial state
+    theta0 = np.pi / 2
+    robot_state = x_ref[0], y_ref[0], theta0
+    # 로봇이 도달해야 하는 좌표, world frame을 갖는다. x_ref, y_ref에서 offset 뺀값
+    z0 = dd_helper.ptP_to_ptC(robot_state, params)
     z0 = np.hstack([z0, theta0])
-    # theta 추가 - 이건 ㅇㄹ 
     z = z0
-    # x, y, theta at the start for point C
-    # z0에는 offset 적용되어 있다.
-    # z0 = np.array([x0, y0, theta0]); 
-    # z0 = np.array(z0)
 
-    # 도달해야 할 point들의 arr 준비
-    # p에는 offset 적용이 되어 있지 않음
+    # 로봇이 이동하면서 도달한 위치를 저장할 것임
     traj = np.array([x_ref[0], y_ref[0]])
-    # error 값들이 쌓이게 될 arr
+
+    # plot items - position error & control signals 
     e = np.array([0,0])
-    # control signals 
-    v = [];
-    omega = [];
+    v = [0.0];
+    omega = [0.0];
 
     for i in range(0,len(t)-1):
-    # for i in range(0, 2):
         # 1. get x_c, y_c position
-        x_c, y_c, theta = z0;
+        x_c, y_c, theta = z0
 
-        # 2. get x_p, y_p from x_c,y_c
-        # z0에는 offset 적용되어 있다.
-        # 결국 다시 world frame으로 바꾸는 것임 
-        # 보통 p0는 센서가 담당한다.
-        x_p, y_p = ptC_to_ptP(z0[0], z0[1], z0[2], params)
+        # 2. get x_p, y_p from x_c,y_c z0에는 offset 적용되어 있다.
+        # 결국 다시 world frame으로 바꾸는 것임 (보통 p0는 센서가 담당한다.)
+        x_p, y_p = dd_helper.ptC_to_ptP(z0, params)
+        # 지금은 아래와 완전 동일함
+        # x_p, y_p = x_ref[i], y_ref[i]
 
-        #% 3. get error = xe-x_ref and ye-y_ref
-        error = [x_ref[i+1]-x_p, y_ref[i+1]-y_p]
-        # e = np.vstack([e, error])
+        # 3. get error
+        error = [x_ref[i+1] - x_p, y_ref[i+1] - y_p]
+        e = np.vstack([e, error])
 
-        # %4. get u = [v, omega] from the errors
-        b = [params.Kp*error[0], params.Kp*error[1]]
-        cos = np.cos(theta);
-        sin = np.sin(theta);
-
+        # 4. get u = [v, omega] from the errors
+        b = [ 10.1 + params.Kp * error[0], 0.1 + params.Kp * error[1]]
         px, py = params.px, params.py
+
+        cos = np.cos(theta)
+        sin = np.sin(theta)
         Ainv = np.array([
             [cos-(py/px)*sin, sin+(py/px)*cos],
             [-(1/px)*sin,     (1/px)*cos     ]
         ])
-        # u = (v, w)
         u = np.matmul(Ainv, np.transpose(b))
-        #u = Ainv*b; #%u = [v omega]
+
         v.append(u[0])
         omega.append(u[1])
 
         # 5. now control the car based on u = [v omega]
-        z0 = euler_integration([t[i], t[i+1]],z0,[u[0],u[1]])
+        # offset 적용된 z0로 euler_integration 계산
+        z0 = dd_helper.euler_integration([t[i], t[i+1]],z0,[u[0],u[1]])
         z = np.vstack([z, z0])
-        p0 = ptC_to_ptP(z0[0],z0[1],z0[2],params);
+
+        # offset 적용해제한 뒤 trag에 저장 => 지금은 무조건 같게 됨
+        p0 = dd_helper.ptC_to_ptP(z0, params);
         traj = np.vstack([traj, p0])
 
-    #interpolation
-    t_interp = np.arange(0 , params.t_length , 1/params.fps)
-    f_z1 = interpolate.interp1d(t, z[:,0])
-    f_z2 = interpolate.interp1d(t, z[:,1])
-    f_z3 = interpolate.interp1d(t, z[:,2])
-    shape = (len(t_interp),3)
-    z_interp = np.zeros(shape)
-    z_interp[:,0] = f_z1(t_interp)
-    z_interp[:,1] = f_z2(t_interp)
-    z_interp[:,2] = f_z3(t_interp)
-
-    f_p1 = interpolate.interp1d(t, traj[:,0])
-    f_p2 = interpolate.interp1d(t, traj[:,1])
-    shape = (len(t_interp),2)
-    p_interp = np.zeros(shape)
-    p_interp[:,0] = f_p1(t_interp)
-    p_interp[:,1] = f_p2(t_interp)
-
-    animate(t_interp,z_interp,p_interp,params)
-
+    return z, traj, (e, v, omega)
 
 if __name__=="__main__":
     params = parameters()
     # "astroid" or "circle"
-    path  = generate_path(params, path_type="astroid", show_path=False)
-    motion_simulation(params, path)
+    path  = generate_path(params, path_type="astroid", show_path=True)
+    
+    try:
+        # pre calculate motion states
+        robot_endpoint, traj, plot_data = motion_simulation(params, path)
+    except Exception as e:
+        print(e)
+    finally:
+        # interpolation for animaltion
+        t_interp, z_interp, p_interp = dd_helper.interpolation(params, robot_endpoint, traj)
+        # draw motion
+        animate(params, t_interp,z_interp,p_interp, plot_data)
 
-    pass
+        print("Everything done!")
