e7f3220e9ede6b39fe2994944335c46d7d65be04
kimsooyoung <tge1375@naver.com>
2023-01-24 21:57:28 +0900

    lec4 done

--- lec5_sympy/example_derive_projectile.py
+++ lec5_sympy/example_derive_projectile.py
@@ -1,64 +1,59 @@
 import sympy as sy
 
-x,y  = sy.symbols('x y', real=True)
-xdot,ydot  = sy.symbols('xdot ydot', real=True)
-xddot,yddot  = sy.symbols('xddot yddot', real=True)
+x,y = sy.symbols("x y", real=True)
+x_d, y_d = sy.symbols("x_d y_d", real=True)
+x_dd, y_dd = sy.symbols("x_dd y_dd", real=True)
 m,c,g  = sy.symbols('m c g', real=True)
 
-#1) position and velocity
-#positions are x and y
-#velocities are xdot and yddot
+v = sy.sqrt(x_d ** 2 + y_d ** 2)
+Fx = -c * x_d * v
+Fy = -c * y_d * v
 
-#2) Kinetic and potential energy
-T = 0.5*m*(xdot*xdot + ydot*ydot)
-V = m*g*y
-L = T-V
-# print(type(T))
-# print(type(V))
-# print(type(L))
+T = 0.5 * m * (x_d ** 2 + y_d ** 2)
+V = m * g * y
+L = T - V
 
-#3) Euler-Lagrange equations
-v = sy.sqrt(xdot*xdot + ydot*ydot)
-Fx = -c*xdot*v
-Fy = -c*ydot*v
+dL_dx_d = sy.diff(L, x_d)
+dt_dL_dx_d = sy.diff(dL_dx_d, x) * x_d + \
+             sy.diff(dL_dx_d, x_d) * x_dd + \
+             sy.diff(dL_dx_d, y) * y_d + \
+             sy.diff(dL_dx_d, y_d) * y_dd
+dL_dx   = sy.diff(L, x)
+EOM1 = dt_dL_dx_d - dL_dx - Fx
+print(EOM1)
 
-#4a) EOM: Manually writing down all terms
-# Since L(x,y,xdot,ydot)
-dLdxdot = sy.diff(L,xdot)
-ddt_dLdxdot = sy.diff(dLdxdot,x)*xdot + sy.diff(dLdxdot,xdot)*xddot + \
-              sy.diff(dLdxdot,y)*ydot + sy.diff(dLdxdot,ydot)*yddot
-dLdx = sy.diff(L,x)
-EOM1 = ddt_dLdxdot - dLdx -Fx
-# print(EOM1)
+dL_dy_d = sy.diff(L, y_d)
+dt_dL_dy_d = sy.diff(dL_dy_d, x) * x_d + \
+             sy.diff(dL_dy_d, x_d) * x_dd + \
+             sy.diff(dL_dy_d, y) * y_d + \
+             sy.diff(dL_dy_d, y_d) * y_dd
+dL_dy = sy.diff(L, y)
+EOM2 = dt_dL_dy_d - dL_dy - Fy
+print(EOM2)
 
-dLdydot = sy.diff(L,ydot)
-ddt_dLdydot = sy.diff(dLdydot,x)*xdot + sy.diff(dLdydot,xdot)*xddot + \
-              sy.diff(dLdydot,y)*ydot + sy.diff(dLdydot,ydot)*yddot
-dLdy = sy.diff(L,y)
-EOM2 = ddt_dLdydot - dLdy -Fy
-
-print(sy.solve(EOM1,xddot))
-print(sy.solve(EOM2,yddot))
+print(sy.solve(EOM1, x_dd))
+print(sy.solve(EOM2, y_dd))
 print('\n')
 
-#4a) EOM: Using loops to automate equation generation
 q = [x, y]
-qdot = [xdot, ydot]
-qddot = [xddot, yddot]
+q_d = [x_d, y_d]
+q_dd = [x_dd, y_dd]
 F = [Fx, Fy]
-dLdqdot = []
-ddt_dLdqdot = []
-dLdq = []
-EOM = []
-mm = len(qddot)
-for i in range(0,mm):
-    dLdqdot.append(sy.diff(L,qdot[i]))
-    tmp = 0;
-    for j in range(0,mm):
-        tmp += sy.diff(dLdqdot[i],q[j])*qdot[j]+ sy.diff(dLdqdot[i],qdot[j])*qddot[j]
-    ddt_dLdqdot.append(tmp)
-    dLdq.append(sy.diff(L,q[i]));
-    EOM.append(ddt_dLdqdot[i] - dLdq[i] - F[i])
 
-print(sy.solve(EOM[0],xddot))
-print(sy.solve(EOM[1],yddot))
+dL_dq_d = []
+dt_dL_dq_d = []
+dL_dq = []
+EOM = []
+
+for i in range(len(q)):
+    dL_dq_d.append(sy.diff(L, q_d[i]))
+    temp_symbol = 0
+    for j in range(len(q)):
+        temp_symbol += sy.diff(dL_dq_d[i], q[j]) * q_d[j] + sy.diff(dL_dq_d[i], q_d[j]) * q_dd[j]
+    
+    dt_dL_dq_d.append(temp_symbol)
+    dL_dq.append(sy.diff(L, q[i]))
+    EOM.append(dt_dL_dq_d[i] - dL_dq[i] - F[i])
+
+print(sy.solve(EOM[0], x_dd))
+print(sy.solve(EOM[1], y_dd))
---
+++ lec5_sympy/test.py
@@ -0,0 +1,66 @@
+import sympy as sy
+
+x,y  = sy.symbols('x y', real=True)
+xdot,ydot  = sy.symbols('xdot ydot', real=True)
+xddot,yddot  = sy.symbols('xddot yddot', real=True)
+m,c,g  = sy.symbols('m c g', real=True)
+
+#1) position and velocity
+#positions are x and y
+#velocities are xdot and yddot
+
+#2) Kinetic and potential energy
+T = 0.5*m*(xdot*xdot + ydot*ydot)
+V = m*g*y
+L = T-V
+# print(type(T))
+# print(type(V))
+# print(type(L))
+
+#3) Euler-Lagrange equations
+v = sy.sqrt(xdot*xdot + ydot*ydot)
+Fx = -c*xdot*v
+Fy = -c*ydot*v
+
+#4a) EOM: Manually writing down all terms
+# Since L(x,y,xdot,ydot)
+dLdxdot = sy.diff(L,xdot)
+ddt_dLdxdot = sy.diff(dLdxdot,x)*xdot + sy.diff(dLdxdot,xdot)*xddot + \
+              sy.diff(dLdxdot,y)*ydot + sy.diff(dLdxdot,ydot)*yddot
+dLdx = sy.diff(L,x)
+EOM1 = ddt_dLdxdot - dLdx -Fx
+print(f"EOM1 : {EOM1}")
+
+dLdydot = sy.diff(L,ydot)
+ddt_dLdydot = sy.diff(dLdydot,x)*xdot + sy.diff(dLdydot,xdot)*xddot + \
+              sy.diff(dLdydot,y)*ydot + sy.diff(dLdydot,ydot)*yddot
+dLdy = sy.diff(L,y)
+EOM2 = ddt_dLdydot - dLdy -Fy
+print(f"EOM2 : {EOM2}")
+
+
+print(sy.solve(EOM1,xddot))
+print(sy.solve(EOM2,yddot))
+print('\n')
+
+#4a) EOM: Using loops to automate equation generation
+q = [x, y]
+qdot = [xdot, ydot]
+qddot = [xddot, yddot]
+F = [Fx, Fy]
+dLdqdot = []
+ddt_dLdqdot = []
+dLdq = []
+EOM = []
+mm = len(qddot)
+for i in range(0,mm):
+    dLdqdot.append(sy.diff(L,qdot[i]))
+    tmp = 0;
+    for j in range(0,mm):
+        tmp += sy.diff(dLdqdot[i],q[j])*qdot[j]+ sy.diff(dLdqdot[i],qdot[j])*qddot[j]
+    ddt_dLdqdot.append(tmp)
+    dLdq.append(sy.diff(L,q[i]));
+    EOM.append(ddt_dLdqdot[i] - dLdq[i] - F[i])
+
+print(sy.solve(EOM[0],xddot))
+print(sy.solve(EOM[1],yddot))
