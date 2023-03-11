ab0789fe73b68195ecb2b326438a6c7eb5498f9f
kimsooyoung <tge1375@naver.com>
2023-03-12 08:25:05 +0900

    lec 11 added
    Signed-off-by: kimsooyoung <tge1375@naver.com>

---
+++ lec10_optimization/traj_opt_car_collocation1.py
@@ -0,0 +1,154 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import math
+from scipy import interpolate
+from scipy.integrate import odeint
+import random
+import scipy.optimize as opt
+from copy import deepcopy
+
+class parameters:
+    def __init__(self):
+        # D : distance between start and end
+        # N : number of collocation points
+        self.D = 5
+        self.N = 12
+
+# cost function : Time 즉, x[0]
+def cost(x):
+    return x[0]
+
+def nonlinear_fn(x):
+    parms = parameters()
+    D = parms.D
+    N = parms.N
+
+    T = x[0];
+    t = np.linspace(0,T,N+1)
+    dt = t[1]-t[0]
+    
+    # x에서 state들 추출
+    pos = [0] * (N+1)
+    vel = [0] * (N+1)
+    u = [0] * (N+1)
+    for i in range(0, N+1):
+        pos[i] = x[i+1]
+        vel[i] = x[i+1 + N+1]
+        u[i] = x[i+1 + N+1 + N+1]
+
+
+    defect_pos = [0] * N
+    defect_vel = [0] * N
+    for i in range(0,N):
+        defect_pos[i] = pos[i+1] - pos[i] - vel[i]*dt;
+        defect_vel[i] = vel[i+1] - vel[i] - 0.5*(u[i]+u[i+1])*dt;
+
+    ceq = [0] * (2*N + 4)
+    
+    ceq[0] = pos[0]
+    ceq[1] = vel[0]
+    ceq[2] = pos[N] - D
+    ceq[3] = vel[N]
+    for i in range(0,N):
+       ceq[i+4] = defect_pos[i]
+       ceq[i+N+4] = defect_vel[i]
+
+    return ceq
+
+def plot(T, pos, vel, u, N):
+    
+    t = np.linspace(0, T, N+1)
+    plt.figure(1)
+
+    plt.subplot(3,1,1)
+    plt.plot(t, pos)
+    plt.ylabel("x")
+
+    plt.subplot(3,1,2)
+    plt.plot(t,vel)
+    plt.ylabel("xdot")
+
+    plt.subplot(3,1,3)
+    plt.plot(t,u)
+    plt.ylabel("u")
+
+    plt.xlabel("t")
+
+    plt.show(block=False)
+    plt.pause(10)
+    plt.close()
+
+
+if __name__ == "__main__":
+
+    random.seed(1)
+    
+    # define parameters
+    parms = parameters()
+    N = parms.N
+    D = parms.D
+
+    # define constraints
+    T_min, T_max = 1, 5
+    pos_min, pos_max = 0, D
+    vel_min, vel_max = -10, 10
+    u_min, u_max = -5, 5
+
+    # define initial guess
+    # 일단 아무 숫지만 넣은 것이다.
+    # 최적 시간 2, 최적 위치 0, 최적 속도 0, 최적 토크 0
+    T_opt = 2
+    pos_opt = vel_opt = u_opt = [0] * (N+1)
+
+    # x0 : 모든 state의 initial guess
+    # x0 = [ T_opt, pos_opt, vel_opt, u_opt ]
+    x0 = [0] * (1+N+1+N+1+N+1)
+    x_min = deepcopy(x0)
+    x_max = deepcopy(x0)
+    
+    # # 이제 모두 0이었던 x0에 x_min, x_max를 채워넣자
+    x0[0], x_min[0], x_max[0] = T_opt, T_min, T_max
+    
+    for i in range(1,1+N+1):
+        x0[i] = pos_opt[i-1]
+        x_min[i] = pos_min
+        x_max[i] = pos_max
+    for i in range(1+N+1,1+N+1+N+1):
+        x0[i] = vel_opt[i-1-N-1]
+        x_min[i] = vel_min
+        x_max[i] = vel_max
+    for i in range(1+N+1+N+1,1+N+1+N+1+N+1):
+        x0[i] = u_opt[i-1-N-1-N-1]
+        x_min[i] = u_min
+        x_max[i] = u_max
+
+    limits = opt.Bounds(x_min, x_max)
+
+    eq_cons = {'type': 'eq',
+            'fun' : nonlinear_fn
+            }
+
+    res = opt.minimize(cost, x0, method='SLSQP',
+                constraints=[eq_cons], options={'ftol': 1e-12, 'disp': True, 'maxiter':500},
+                bounds=limits)
+    
+    # analyze result
+    print(x0)
+    print(res.x)
+    print(res.success)
+    print(res.message)
+    print(res.status)
+
+    x = res.x
+    
+    # T, pos, vel, u 빼내기
+    T = x[0]
+    pos = [0]* (N+1)
+    vel = [0]* (N+1)
+    u = [0] * (N+1)
+    for i in range(0,N+1):
+        pos[i] = x[i +1]
+        vel[i] = x[i +1+N+1]
+        u[i] = x[i +1+N+1+N+1]
+    
+    plot(T, pos, vel, u, N)
---
+++ lec10_optimization/traj_opt_car_shooting2.py
@@ -0,0 +1,127 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import math
+from scipy import interpolate
+from scipy.integrate import odeint
+import random
+import scipy.optimize as opt
+
+class parameters:
+    def __init__(self):
+        self.D = 5
+        self.N = 5
+        self.z0 = np.array([0,0])
+
+def car(z, t, t1,t2,u1,u2):
+
+    xdot = z[1];
+
+    t_opt = np.array([t1,t2])
+    u_opt = np.array([u1,u2])
+    if (t>t2 or t<= t1): #needed because odeint goes outside t bounds
+        u = 0
+    else:
+        f = interpolate.interp1d(t_opt, u_opt)
+        u = f(t)
+
+    dzdt = np.array([xdot, u]);
+    return dzdt
+
+def simulator(x,z0,N):
+    T = x[0];
+    for i in range(0,N+1):
+        u_opt[i] = x[i+1]
+
+    t_opt = np.linspace(0,T,N+1)
+
+    shape = (N+1,2)
+    zz = np.zeros(shape)
+    zz[0,0] = z0[0]
+    zz[0,1] = z0[1]
+    tt = t_opt
+    uu = u_opt
+    for i in range(0,N): #goes from 0 to N
+        args = (t_opt[i],t_opt[i+1],u_opt[i],u_opt[i+1])
+        z = odeint(car, z0, np.array([t_opt[i],t_opt[i+1]]), args,rtol=1e-12,atol=1e-12)
+        z0 = np.array([z[1,0], z[1,1]]);
+        zz[i+1,0] = z0[0]
+        zz[i+1,1] = z0[1]
+
+    return tt,zz,uu
+
+def cost(x):
+    return x[0]
+
+def nonlinear_fn(x):
+    parms = parameters()
+    D = parms.D
+    z0 = parms.z0
+    N = parms.N
+
+    [tt,zz,uu] = simulator(x,z0,N);
+    x_end = zz[N,0]-D;
+    v_end = zz[N,1];
+    return [x_end, v_end ]
+
+random.seed(1)
+parms = parameters()
+N = parms.N
+
+T_min = 1; T_max = 5;
+u_min = -5; u_max = 5;
+
+T_opt = 1
+u_opt = [0] * (N+1) #initialize u to zeros
+for i in range(0,N+1):
+    u_opt[i] = u_min + (u_max-u_min)*random.random();
+
+x0 = [0] * (N+2)
+x_min = [0] * (N+2)
+x_max = [0] * (N+2)
+x0[0] = T_opt
+x_min[0] = T_min
+x_max[0] = T_max
+for i in range(1,N+2):
+    x0[i] = u_opt[i-1]
+    x_min[i] = u_min
+    x_max[i] = u_max
+
+
+limits = opt.Bounds(x_min,
+                    x_max)
+
+nonlinear_constraint = opt.NonlinearConstraint(nonlinear_fn, [0, 0], [0,0])
+
+res = opt.minimize(cost, x0, method='trust-constr',
+               constraints=[nonlinear_constraint],
+               options={'verbose': 1,'maxiter':500}, bounds=limits)
+print(x0)
+x = res.x
+print(x)
+print(res.success)
+print(res.message)
+print(res.status)
+
+#testing
+#x = np.array([ 3.84685576, -1.72830045,  3.79629776,  1.51420446, -3.4370371,  -0.05651076, -1.90560663])
+# x = np.array([2.16628567,  4.06594116,  4.27343727,  4.66086665, -4.37819845, -4.46133521,-4.2555821])
+z0 = parms.z0
+[tt,zz,uu] = simulator(x,z0,N) #test
+print(zz[N,0])
+print(zz[N,1])
+
+plt.figure(1)
+plt.subplot(3,1,1)
+plt.plot(tt,zz[:,0])
+plt.ylabel("x")
+plt.subplot(3,1,2)
+plt.plot(tt,zz[:,1])
+plt.ylabel("xdot")
+plt.subplot(3,1,3)
+plt.plot(tt,uu)
+plt.xlabel("t")
+plt.ylabel("u")
+#plt.show()
+plt.show(block=False)
+plt.pause(4)
+plt.close()
