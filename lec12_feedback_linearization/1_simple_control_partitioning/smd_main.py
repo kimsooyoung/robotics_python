cb73589413c1c1182f63a41f9d3a43b659f79afb
kimsooyoung <tge1375@naver.com>
2023-03-19 10:04:23 +0900

    1d done
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- lec12_feedback_linearization/1_simple_control_partitioning/smd_main.py
+++ lec12_feedback_linearization/1_simple_control_partitioning/smd_main.py
@@ -1,17 +1,20 @@
 from matplotlib import pyplot as plt
-import numpy as np
-import math
 from scipy.integrate import odeint
+
+import numpy as np
 
 class parameters:
     def __init__(self):
+        # system parameters
         self.m = 1
         self.c = 1
         self.k = 1
+        
+        # control gains
         self.kp = 10
         self.kd = 1*(-self.c+2*np.sqrt(self.m*(self.k+self.kp)));
 
-def smd_rhs(z,t,m,c,k,kp,kd):
+def smd_rhs(z,t, m, c, k, kp, kd):
 
     x = z[0];
     xdot = z[1];
@@ -26,22 +29,29 @@ def smd_rhs(z,t,m,c,k,kp,kd):
 
     return zdot
 
-parms = parameters()
-x0 = 0.5;
-xdot0 = 0;
-t0 = 0;
-tend = 20;
+def plot(t, z):
+    
+    plt.figure(1)
+    
+    plt.plot(t,z[:,0])
+    plt.xlabel("t")
+    plt.ylabel("position")
+    plt.title("Plot of position vs time")
+    
+    plt.show()
+    
 
-t = np.linspace(t0, tend, 101)
-z0 = np.array([x0, xdot0])
-z = odeint(smd_rhs, z0, t, args=(parms.m,parms.c,parms.k,parms.kp,parms.kd))
+if __name__ == "__main__":
+    params = parameters()
+    
+    x0, xdot0 = 0.5, 0
+    t0, tend = 0, 20
+    
+    t = np.linspace(t0, tend, 101)
+    z0 = np.array([x0, xdot0])
+    
+    # Extra arguments must be in a tuple.
+    z = odeint(smd_rhs, z0, t, args=(params.m,params.c,params.k,params.kp,params.kd))
 
-plt.figure(1)
-plt.plot(t,z[:,0])
-plt.xlabel("t")
-plt.ylabel("position")
-plt.title("Plot of position vs time")
-plt.show()
-# plt.show(block=False)
-# plt.pause(2)
-# plt.close()
+    plot(t, z)
+    
