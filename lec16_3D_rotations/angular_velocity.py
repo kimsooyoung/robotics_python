6b4335b0af65aafe54529caa00bed1e476bedb12
kimsooyoung <tge1375@naver.com>
2023-04-05 09:30:06 +0900

    ang vel added
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- lec16_3D_rotations/angular_velocity.py
+++ lec16_3D_rotations/angular_velocity.py
@@ -22,11 +22,11 @@ R_x = sy.Matrix([ [1, 0, 0],
                   [0, sin(phi),   cos(phi)] ])
 
 R_y = sy.Matrix( [ [cos(theta),  0,   sin(theta)],
-                   [0,          1,         0     ],
-                   [-sin(theta), 0,   cos(theta)] ])
+                   [0,          1,         0    ],
+                   [-sin(theta), 0,   cos(theta)]])
 
 R_z = sy.Matrix( [ [cos(psi), -sin(psi),   0],
-                   [sin(psi),  cos(psi),   0     ],
+                   [sin(psi),  cos(psi),   0],
                    [0,           0,         1] ])
 
 om = psidot*k + R_z*(thetadot*j)+ R_z*R_y*(phidot*i)
