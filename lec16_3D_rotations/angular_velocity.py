aad9d19401fb60591eb538553f5036106e2f2765
kimsooyoung <tge1375@naver.com>
2023-04-05 10:18:00 +0900

    ang vel done
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- lec16_3D_rotations/angular_velocity.py
+++ lec16_3D_rotations/angular_velocity.py
@@ -1,5 +1,4 @@
 import sympy as sy
-import numpy as np
 
 def sin(angle):
     return sy.sin(angle)
@@ -29,12 +28,22 @@ R_z = sy.Matrix( [ [cos(psi), -sin(psi),   0],
                    [sin(psi),  cos(psi),   0],
                    [0,           0,         1] ])
 
-om = psidot*k + R_z*(thetadot*j)+ R_z*R_y*(phidot*i)
 
-R_we = om.jacobian([phidot,thetadot,psidot])
+q_d = sy.Matrix([phidot,thetadot,psidot])
+
+
+om = psidot*k + R_z*(thetadot*j)+ R_z*R_y*(phidot*i)
+# 하나의 기법으로 표현
+# 결국 구하는 것은 R_w, R_wb 행렬이다.
+# w = R_we * q_dot임을 시용해서 R_we만 뽑아내는 것이다.
+# R_we = om.jacobian([phidot,thetadot,psidot])
+R_we = om.jacobian(q_d)
+
+print(sy.simplify(R_we))
 print(sy.simplify(R_we.det()))
 
 om_b = phidot*i +  R_x.transpose()*(thetadot*j) + R_x.transpose()*R_y.transpose()*(psidot*k);
-R_be = om_b.jacobian([phidot,thetadot,psidot]);
+R_be = om_b.jacobian(q_d)
 
+print(sy.simplify(R_be))
 print(sy.simplify(R_be.det()))
