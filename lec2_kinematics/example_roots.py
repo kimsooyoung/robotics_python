9d6317af2c5c735845e0520598d53f4190fad178
kimsooyoung <tge1375@naver.com>
2023-04-13 11:33:32 +0900

    doc ongoing
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- lec2_kinematics/example_roots.py
+++ lec2_kinematics/example_roots.py
@@ -5,17 +5,23 @@ import numpy as np
 def func(x):
     return x**2 - x - 2
 
+# # case 1
+# root = fsolve(func, 3)
+# case 2
 root = fsolve(func, 0)
-print(root)
 
 x = np.arange(-6,6,0.1)   # start,stop,step
 y = func(x)
 
 plt.figure(1)
+
 plt.plot(x,y)
+plt.plot([-6, 6], [0, 0], color="black", linewidth=1)
+plt.plot(root, 0, color="green", marker="o", markersize=10)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Plot of func")
+
 plt.grid()
 plt.show(block=False)
 plt.pause(5)
