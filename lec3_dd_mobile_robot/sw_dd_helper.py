e2a2ce47071adefe19aeb3ab29762902920e5989
kimsooyoung <tge1375@naver.com>
2023-04-18 11:01:52 +0900

    new version during doc
    Signed-off-by: kimsooyoung <tge1375@naver.com>

---
+++ lec3_dd_mobile_robot/sw_dd_helper.py
@@ -0,0 +1,92 @@
+import math
+import numpy as np
+from scipy import interpolate
+
+def euler_integration(tspan,z0,u):
+    v = u[0]
+    omega = u[1]
+    h = tspan[1]-tspan[0]
+
+    x0 = z0[0]
+    y0 = z0[1]
+    theta0 = z0[2]
+
+    xdot_c = v*math.cos(theta0)
+    ydot_c = v*math.sin(theta0)
+    thetadot = omega
+
+    x1 = x0 + xdot_c*h
+    y1 = y0 + ydot_c*h
+    theta1 = theta0 + thetadot*h
+
+    z1 = [x1, y1, theta1]
+    return z1
+
+def world_to_robot(state, params):
+    x_p, y_p, theta = state
+    cos = np.cos(theta)
+    sin = np.sin(theta)
+    
+    H_robot_world = np.array([
+        [cos, -sin, x_p],
+        [sin, cos, y_p],
+        [0, 0, 1]
+    ])
+    
+    c = np.array([-params.px, -params.py, 1])
+    
+    return np.matmul(H_robot_world, c)
+    
+# world frame point after robot frame movement "p"
+def robot_to_world(state, params):
+    x_c, y_c, theta = state
+    cos = np.cos(theta)
+    sin = np.sin(theta)
+    
+    H_world_robot = np.array([
+        [cos, -sin, x_c],
+        [sin, cos, y_c],
+        [0, 0, 1]
+    ])
+    
+    p = np.array([params.px, params.py, 1])
+    
+    return np.matmul(H_world_robot, p)
+
+def interpolation(params, z):
+
+    #interpolation
+    t = np.arange(0, params.t_end, 0.01)
+    
+    t_interp = np.arange(0 , params.t_end , 1/params.fps)
+    f_z1 = interpolate.interp1d(t, z[:,0])
+    f_z2 = interpolate.interp1d(t, z[:,1])
+    f_z3 = interpolate.interp1d(t, z[:,2])
+    
+    z_interp = np.zeros((len(t_interp),3))
+    z_interp[:,0] = f_z1(t_interp)
+    z_interp[:,1] = f_z2(t_interp)
+    z_interp[:,2] = f_z3(t_interp)
+
+    return t_interp, z_interp
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# x = np.arange(5)
+# y = np.exp(x)
+# fig1, ax1 = plt.subplots()
+# ax1.plot(x, y)
+# ax1.set_title("Axis 1 title")
+# ax1.set_xlabel("X-label for axis 1")
+
+# z = np.sin(x)
+# fig2, (ax2, ax3) = plt.subplots(nrows=2, ncols=1) # two axes on figure
+# ax2.plot(x, z)
+# ax3.plot(x, -z)
+
+# w = np.cos(x)
+# ax1.plot(x, w) # can continue plotting on the first axis
+
+# plt.show()
---
+++ lec3_dd_mobile_robot/sw_diff_drive_ik.py
@@ -0,0 +1,169 @@
+from matplotlib import pyplot as plt
+import math
+import sw_dd_helper
+import numpy as np
+
+class parameters:
+    def __init__(self):
+        # 목적지에 도달해야 하는 offset
+        # 목적지와 딱 맞게 가고 싶다면 px=0.0이어야 하는건가? => yes
+        self.px = 0.01
+        self.py = 0.0
+        self.Kp = 100;
+        # 로봇 반지름
+        self.R = 0.1
+        self.pause = 0.1
+        self.fps = 5
+        self.t_end = 10
+
+def animate(params, t_interp, z_interp, z_dot, err):
+
+    R = params.R;
+    phi = np.arange(0,2*np.pi,0.25)
+    x_circle = R*np.cos(phi)
+    y_circle = R*np.sin(phi)
+
+    for i in range(0,len(t_interp)):
+        x = z_interp[i,0]
+        y = z_interp[i,1]
+        theta = z_interp[i,2]
+
+        x_robot = x + x_circle
+        y_robot = y + y_circle
+
+        x2 = x + R*np.cos(theta)
+        y2 = y + R*np.sin(theta)
+
+        # 로봇 방향을 나타내는 작대기
+        line, = plt.plot([x, x2],[y, y2],color="black")
+        robot,  = plt.plot(x_robot,y_robot,color='black')
+        # 로봇이 그리는 경로
+        # shape, = plt.plot(p[0:i,0],p[0:i,1],color='red');
+        shape, = plt.plot(z_interp[0:i,0], z_interp[0:i,1], color='red', marker="o", markersize=0.5);
+
+        plt.xlim(-2,2)
+        plt.ylim(-2,2)
+        plt.gca().set_aspect('equal')
+        plt.pause(params.pause)
+        
+        line.remove()
+        robot.remove()
+        shape.remove()
+
+    fig3, (ax3, ax4, ax5) = plt.subplots(nrows=3, ncols=1)
+    # e, v, omega = plot_items
+    t = np.arange(0, params.t_end, 0.01)
+    ax3.set_title("Error (x_ref - x_p)")
+    ax3.plot(t, err[:, 0], color="green", label='X err')
+    ax3.plot(t, err[:, 1], color="orange", label='Y err')
+    ax3.legend(loc="upper right")
+
+    ax4.plot(t, z_dot[:,0], color="blue")
+    ax4.set_title("Control Signal - V")
+
+    ax5.plot(t, z_dot[:,1], color="red")
+    ax5.set_title("Control Signal - W")
+
+    plt.show()
+
+def generate_path(params, path_type="astroid", show_path=False):
+    t0 = 0
+    tend = params.t_end
+    t = np.arange(t0,tend,0.01)
+
+    if path_type == "circle":
+        R = 1.0
+        x_ref = R * np.cos(2 * math.pi * t/tend)
+        y_ref = R * np.sin(2 * math.pi * t/tend)
+    # generate astroid-shape path 
+    # (note these are coordinates of point P)
+    elif path_type == "astroid":
+        x_center = 0;
+        y_center = 0;
+        a = 1;
+        x_ref = x_center + a * np.cos(2 * math.pi * t/tend) **3
+        y_ref = y_center + a * np.sin(2 * math.pi * t/tend) **3
+        
+    if show_path == True:
+        fig1, ax1 = plt.subplots()
+        ax1.plot(x_ref,y_ref)
+        ax1.set_title("Object Path")
+
+        plt.xlim(-2,2)
+        plt.ylim(-2,2)
+        plt.gca().set_aspect('equal')
+
+    return x_ref, y_ref
+
+def motion_simulation(params, path):
+
+    px, py = params.px, params.py
+
+    x_ref, y_ref = path
+    t = np.arange(0, params.t_end, 0.01)
+
+    # initial state
+    z0 = np.array([x_ref[0], y_ref[0], np.pi/2])
+    z = np.zeros((len(t), 3))
+    z[0] = z0
+
+    # plot items - position error & control signals 
+    e = np.zeros((len(t), 2))
+    e[0] = ([0.0, 0.0])
+    
+    # store v, w 
+    z_dot = np.zeros((len(t), 2))
+    z_dot[0] = ([0.0, 0.0])
+    
+    for i in range(0,len(t)-1):
+        # 1. cur robot state in world frame
+        _, _, theta = z0
+        
+        # 2. x_c, y_c : world frame point after robot movement P
+        x_c, y_c, _ = sw_dd_helper.robot_to_world(z0, params)
+
+        # 3. get error
+        error = [x_ref[i+1] - x_c, y_ref[i+1] - y_c]
+        e[i+1] = error
+
+        # 4. get u = [v, omega] from the errors
+        # b = [ 10.0 + params.Kp * error[0], 10.0 + params.Kp * error[1]]
+        b = [ params.Kp * error[0], params.Kp * error[1]]
+        cos = np.cos(theta)
+        sin = np.sin(theta)
+        Ainv = np.array([
+            [cos-(py/px)*sin, sin+(py/px)*cos],
+            [-(1/px)*sin,     (1/px)*cos     ]
+        ])
+        
+        u = np.matmul(Ainv, np.transpose(b))
+        z_dot[i+1] = u
+
+        # 5. now control the car based on u = [v omega] (euler_integration)
+        z0 = sw_dd_helper.euler_integration([t[i], t[i+1]],z0,[u[0],u[1]])
+        z[i+1] = z0
+        
+    return z, z_dot, e
+
+if __name__=="__main__":
+    params = parameters()
+    # "astroid" or "circle"
+    path = generate_path(params, path_type="astroid", show_path=True)
+    
+    z, z_dot, err = motion_simulation(params, path)
+    
+    t_interp, z_interp = sw_dd_helper.interpolation(params, z)
+    
+    animate(params, t_interp, z_interp, z_dot, err)
+    
+    try:
+        # pre calculate motion states
+        # robot_endpoint, traj, plot_data = motion_simulation(params, path)
+    except Exception as e:
+        print(e)
+    finally:
+        # interpolation for animaltion
+        t_interp, z_interp, p_interp = sw_dd_helper.interpolation(params, robot_endpoint, traj)
+        # draw motion
+        animate(params, t_interp,z_interp,p_interp, plot_data)
+        print("Everything done!")
