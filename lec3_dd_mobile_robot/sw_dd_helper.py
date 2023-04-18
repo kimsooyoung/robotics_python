952fcc36d81b54ac2dd9d8e9fa1973ab36fc147f
kimsooyoung <tge1375@naver.com>
2023-04-18 11:02:38 +0900

    doc
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- lec3_dd_mobile_robot/sw_dd_helper.py
+++ lec3_dd_mobile_robot/sw_dd_helper.py
@@ -73,20 +73,3 @@ def interpolation(params, z):
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-# x = np.arange(5)
-# y = np.exp(x)
-# fig1, ax1 = plt.subplots()
-# ax1.plot(x, y)
-# ax1.set_title("Axis 1 title")
-# ax1.set_xlabel("X-label for axis 1")
-
-# z = np.sin(x)
-# fig2, (ax2, ax3) = plt.subplots(nrows=2, ncols=1) # two axes on figure
-# ax2.plot(x, z)
-# ax3.plot(x, -z)
-
-# w = np.cos(x)
-# ax1.plot(x, w) # can continue plotting on the first axis
-
-# plt.show()
