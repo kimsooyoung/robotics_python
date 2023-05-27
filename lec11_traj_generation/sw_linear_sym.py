f679445389296b2e5b745b85fd478af82a92d590
kimsooyoung <tge1375@naver.com>
2023-05-27 13:19:29 +0900

    lec 11 doc
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- lec11_traj_generation/sw_linear_sym.py
+++ lec11_traj_generation/sw_linear_sym.py
@@ -1,18 +1,24 @@
 import sympy as sy
 
-t0, tf = sy.symbols('t0 tf', real=True)
-q0, qf = sy.symbols('q0 qf', real=True)
+# q0 = a0 + a1*t0
+# qf = a0 + a1*tf
+#
+# Ax = b
 
-A = sy.Matrix([ 
-        [1, t0],
-        [1, tf]
-    ])
+a0, a1 = sy.symbols('a0 a1')
+q0, qf = sy.symbols('q0 qf')
+t0, tf = sy.symbols('t0 tf')
+
+A = sy.Matrix([
+    [1, t0], 
+    [1, tf]
+])
 
 b = sy.Matrix([
-        [q0],
-        [qf]
-    ])
+    [q0],
+    [qf]
+])
 
-ans = A.inv() * b
-print(f"a0 = {ans[0]}")
-print(f"a1 = {ans[1]}")
+x = A.inv() * b
+print(f"a0 = {x[0]}")
+print(f"a1 = {x[1]}")
