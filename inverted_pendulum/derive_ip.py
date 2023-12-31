a2fdb9d87cfa35d25e0b3c79abd846688ab3ac96
kimsooyoung <tge1375@naver.com>
2023-12-31 17:37:48 +0900

    bf contents
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- inverted_pendulum/derive_ip.py
+++ inverted_pendulum/derive_ip.py
@@ -83,16 +83,60 @@ for i in range(len(q_dd)):
     EOM.append(dt_dL_dq_d[i] - dL_dq[i] - Q[i])
 
 EOM = sy.Matrix(EOM)
+print("Normal EOM")
+print('EOM[0] = ', sy.simplify(EOM[0]))
+print('EOM[1] = ', sy.simplify(EOM[1]))
+print()
 
 # Ax = b
-A_ss = EOM.jacobian(q_dd)
+A = EOM.jacobian(q_dd)
 b_temp = []
 
 for i in range(len(q_dd)):
     b_temp.append(-1 * EOM[i].subs([(x_ddot, 0), (theta_ddot, 0)]))
-b_ss = sy.Matrix([b_temp[0], b_temp[1]])
+b = sy.Matrix([b_temp[0], b_temp[1]])
 
-EOM_ss = A_ss.inv() * b_ss
+#######################
+# Step 4. State-Space #
+#######################
+
+EOM_ss = A.inv() * b
+print("State Equation")
 print('EOM_ss[0] = ', sy.simplify(EOM_ss[0]))
 print('EOM_ss[1] = ', sy.simplify(EOM_ss[1]))
+print()
 
+A_ss1 = x_dot
+A_ss2 = EOM_ss[0]
+A_ss3 = theta_dot
+A_ss4 = EOM_ss[1]
+
+A_temp = sy.Matrix([A_ss1, A_ss2, A_ss3, A_ss4])
+q_ss = sy.Matrix([x, x_dot, theta, theta_dot])
+u_ss = sy.Matrix([u])
+
+A_ss = A_temp.jacobian(q_ss)
+B_ss = A_temp.jacobian(u_ss)
+
+#########################
+# Step 5. Linearization #
+#########################
+
+# fixed point 1. => 실제로는 이것 사용
+# x_dot = 0 / theta = 0 / theta_dot = 0
+A_lin_case1 = A_ss.subs([(x_dot, 0), (theta, 0), (theta_dot, 0)])
+B_lin_case1 = B_ss.subs([(x_dot, 0), (theta, 0), (theta_dot, 0)])
+print("A_lin_case1")
+print(sy.simplify(A_lin_case1))
+print("B_lin_case1")
+print(sy.simplify(B_lin_case1))
+print()
+
+# fixed point 2. 
+# x_dot = 0 / theta = sy.pi / theta_dot = 0
+A_lin_case2 = A_ss.subs([(x_dot, 0), (theta, sy.pi), (theta_dot, 0)])
+B_lin_case2 = B_ss.subs([(x_dot, 0), (theta, sy.pi), (theta_dot, 0)])
+print("A_lin_case2")
+print(sy.simplify(A_lin_case2))
+print("B_lin_case2")
+print(sy.simplify(B_lin_case2))
