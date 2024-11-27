017cba6814b3c53b16566d205527054928d434e1
kimsooyoung <tge1375@naver.com>
2024-11-26 20:34:29 -0800

    weighted pendulum ex added

---
+++ lec6_db_pendulum/derive_weight_pendulum.py
@@ -0,0 +1,96 @@
+# Copyright 2024 @RoadBalance
+# Reference from https://pab47.github.io/legs.html
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sympy as sy
+
+# define symbolic quantities
+theta = sy.symbols('theta', real=True)
+m, l, c, I = sy.symbols('m l c I', real=True)
+g = sy.symbols('g', real=True)
+
+# position vectors
+# link1 frame to world frame
+theta_ = 3*sy.pi/2 + theta
+H_01 = sy.Matrix([
+    [sy.cos(theta_), -sy.sin(theta_), 0],
+    [sy.sin(theta_),  sy.cos(theta_), 0],
+    [0, 0, 1]
+])
+
+# Pendulum COM in world frame
+G = H_01 * sy.Matrix([l, 0, 1])
+G_xy = sy.Matrix([G[0], G[1]])
+
+# velocity vectors
+theta_d = sy.symbols('theta_d', real=True)
+q = sy.Matrix([theta])
+q_d = sy.Matrix([theta_d])
+
+v_G = G_xy.jacobian(q) * q_d
+
+# kinetic energy
+T = 0.5*m*v_G.dot(v_G) + 0.5*I*theta_d**2
+# T = 0.5*I*theta_d**2
+
+# potential energy
+V = m*g*G[1]
+
+# Lagrangian
+L = T - V
+
+# Lagrange equation
+theta_dd = sy.symbols('theta_dd', real=True)
+q_dd = sy.Matrix([theta_dd])
+
+dL_dq_d = []
+dt_dL_dq_d = []
+dL_dq = []
+EOM = []
+
+# Derive equations
+for i in range(len(q)):
+    dL_dq_d.append(sy.diff(L, q_d[i]))
+    temp = 0
+    for j in range(len(q)):
+        temp += sy.diff(dL_dq_d[i], q[j]) * q_d[j] + \
+                sy.diff(dL_dq_d[i], q_d[j]) * q_dd[j]
+
+    dt_dL_dq_d.append(temp)
+    dL_dq.append(sy.diff(L, q[i]))
+    # 현재 외력이 0이므로 이 두개 항만 있다.
+    EOM.append(dt_dL_dq_d[i] - dL_dq[i])
+
+EOM = sy.Matrix([EOM])
+print(f"EOM: {EOM}")
+
+# M(q)*q_dd + C(q, q_d)*q_d + G(q) -Tau = 0
+# C : coriolis force
+# G : gravity
+# M(q)*q_dd + C(q, q_d)*q_d + G(q) -Tau = 0
+# b = C(q, q_d)*q_d + G(q) -Tau
+# G = G(q)
+# C = b - G = C(q, q_d)*q_d + G(q) - G(q) = C(q, q_d)*q_d
+M = EOM.jacobian(q_dd)
+b = EOM.subs([
+    (theta_dd, 0)
+])
+G = b.subs([
+    (theta_d, 0),
+])
+C = b - G
+
+print(f"M = {M[0]}")
+print(f"C = {C[0]}")
+print(f"G = {G[0]}")
---
+++ lec6_db_pendulum/main_weight_pendulum.py
@@ -0,0 +1,144 @@
+# Copyright 2022 @RoadBalance
+# Reference from https://pab47.github.io/legs.html
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy import interpolate
+
+from scipy.integrate import odeint
+
+
+class parameters:
+
+    def __init__(self):
+        self.m = 20
+        self.c = 0.5
+        self.l = 1
+        self.I = self.m * self.l * self.l # ml^2
+        self.g = 9.81
+        self.pause = 0.02
+        self.fps = 20
+
+
+def cos(angle):
+    return np.cos(angle)
+
+
+def sin(angle):
+    return np.sin(angle)
+
+
+def interpolation(t, z, params):
+
+    # interpolation
+    t_interp = np.arange(t[0], t[len(t)-1], 1/params.fps)
+    [rows, cols] = np.shape(z)
+    z_interp = np.zeros((len(t_interp), cols))
+
+    for i in range(0, cols):
+        f = interpolate.interp1d(t, z[:, i])
+        z_interp[:, i] = f(t_interp)
+
+    return t_interp, z_interp
+
+
+def animate(t_interp, z_interp, params):
+
+    l = params.l
+    c = params.c
+
+    # plot
+    fig, ax = plt.subplots()
+
+    rect = plt.Rectangle((-0.1, 0.0), 0.2, 0.2, color='brown')
+    ax.add_patch(rect)
+
+    for i in range(0, len(t_interp)):
+
+        theta = z_interp[i, 0]
+
+        O = np.array([0, 0])
+        P = np.array([l*sin(theta), -l*cos(theta)])
+
+        pend, = plt.plot(
+            [O[0], P[0]], [O[1], P[1]], linewidth=5, color='red'
+        )
+
+        com, = plt.plot(
+            P[0], P[1], color='black', marker='o', markersize=30
+        )
+
+        plt.xlim(-2, 2)
+        plt.ylim(-2, 2)
+        plt.gca().set_aspect('equal')
+
+        plt.pause(params.pause)
+
+        if (i < len(t_interp)-1):
+            pend.remove()
+            com.remove()
+
+    plt.show(block=False)
+    plt.pause(1)
+    plt.close()
+
+    # # result plotting
+    # plt.figure(1)
+    # plt.subplot(2, 1, 1)
+    # plt.plot(t, z[:, 0], color='red', label='theta1')
+    # plt.plot(t, z[:, 2], color='blue', label='theta2')
+    # plt.ylabel('angle')
+    # plt.legend(loc='upper left')
+
+    # plt.subplot(2, 1, 2)
+    # plt.plot(t, z[:, 1], color='red', label='omega1')
+    # plt.plot(t, z[:, 3], color='blue', label='omega2')
+    # plt.xlabel('t')
+    # plt.ylabel('angular rate')
+    # plt.legend(loc='lower left')
+
+    # plt.show()
+
+
+def single_pendulum(z0, t, m, I, c, l, g):
+
+    theta, omega = z0
+
+    M = 1.0*I + 0.5*m*(2*l**2*sin(theta)**2 + 2*l**2*cos(theta)**2)
+    C = 0
+    G = g*l*m*sin(theta)
+
+    theta_dd = -(C + G) / M
+
+    return [omega, theta_dd]
+
+
+if __name__ == '__main__':
+
+    params = parameters()
+
+    t = np.linspace(0, 10, 500)
+
+    # initlal state
+    z0 = np.array([np.pi/6, 0.001])
+    all_params = (
+        params.m, params.I,
+        params.c, params.l,
+        params.g
+    )
+    z = odeint(single_pendulum, z0, t, args=all_params)
+
+    t_interp, z_interp = interpolation(t, z, params)
+    animate(t_interp, z_interp, params)
